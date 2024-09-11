09bc4812f5d65a10f3be00fdb7392ec8dcbf1cc5
y0n888 <ayam.k_123@outlook.jp>
2024-09-11 19:10:19 +0900

    samuraimartを追加

---
+++ app/Admin/routes.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Routing\Router;
+use App\Admin\Controllers\CategoryController;
+
+Admin::routes();
+
+Route::group([
+    'prefix'        => config('admin.route.prefix'),
+    'namespace'     => config('admin.route.namespace'),
+    'middleware'    => config('admin.route.middleware'),
+    'as'            => config('admin.route.prefix') . '.',
+], function (Router $router) {
+
+    $router->get('/', 'HomeController@index')->name('home');
+    $router->resource('categories', CategoryController::class);
+
+});
